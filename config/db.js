b017861f631d2095e3b9ddc3f23f743fabab15da
Hari Om <58305594+Madhav008@users.noreply.github.com>
2021-03-01 00:44:43 +0530

    config folder

---
+++ config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+
+const connectDB = async ()=>{
+
+    const conn= await  mongoose.connect(process.env.MongoURI,{
+        useNewUrlParser:true,
+        useCreateIndex:true,
+        useFindAndModify:false,
+        useUnifiedTopology: true 
+
+    });
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
+}
+
+
+module.exports = connectDB;

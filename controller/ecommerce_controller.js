089fcd44761d55120c015f01861c39a0be2560ca
Hari Om <58305594+Madhav008@users.noreply.github.com>
2021-03-19 12:19:31 +0530

    Product detail Edited

--- controller/ecommerce_controller.js
+++ controller/ecommerce_controller.js
@@ -1,22 +1,22 @@
-const EcomProducts = require('../model/Ecommerce')
+const EcomProducts = require("../model/Ecommerce");
 
 //@desc         GET all Ecom Detail
 //@route        GET /api/v1/ecom
 //@acces        Public
 
 exports.getproducts = async (req, res, next) => {
-    const ecomproducts = await EcomProducts.find()
-    res.status(200).json({
-      ecomproducts,
-      success: true,
-      msg: "Show all Details",
-    });
-  };
-  
+  const ecomproducts = await EcomProducts.find();
+  res.status(200).json({
+    ecomproducts,
+    success: true,
+    msg: "Show all Details",
+  });
+};
+
 //@desc         GET all Ecom Detail
 //@route        GET /api/v1/ecom/:id
 //@acces        Public
-  
+
 exports.getproduct = async (req, res, next) => {
   try {
     const companydetails = await EcomProducts.find({ userId: req.params.id });
@@ -32,7 +32,7 @@ exports.getproduct = async (req, res, next) => {
     next(new ErrorResponse("Company Not found "));
   }
 };
-  
+
 //@desc         POST Ecom Detail
 //@route        POST /api/v1/ecom
 //@acces        Private
@@ -40,44 +40,43 @@ exports.getproduct = async (req, res, next) => {
 exports.createproduct = async (req, res, next) => {
   try {
     await EcomProducts.create(req.body),
-    res.status(200).json({
+      res.status(200).json({
         success: true,
         msg: " Created Ecom Detail ",
       });
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
-  
 };
 //@desc         PUT all Ecom Detail
 //@route        PUT /api/v1/ecom/:id
 //@acces        Private
 
 exports.updateproduct = async (req, res, next) => {
-  let ecomproducts = await EcomProducts.find({ userId: req.params.id })
+  let ecomproducts = await EcomProducts.findById(req.params.id);
   try {
     if (!ecomproducts) {
       res.json({
-        msg: "ecom details with this id not found"
-      })
+        msg: "ecom details with this id not found",
+      });
     } else {
-      ecomproducts= await EcomProducts.findOneAndUpdate(
-        {userId: req.params.id }, 
-        req.body, 
+      ecomproducts = await EcomProducts.findOneAndUpdate(
+        { _id: req.params.id },
+        req.body,
         {
           new: true,
           runValidators: true,
-        })
-  
+        }
+      );
+
       res.status(200).json({
         success: true,
         msg: " Update Ecom Detail of" + req.params.id,
       });
     }
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
-  
 };
 
 //@desc         DELETE Ecom Detail
@@ -85,21 +84,20 @@ exports.updateproduct = async (req, res, next) => {
 //@acces        Private
 
 exports.deleteproduct = async (req, res, next) => {
-let ecomproducts = await EcomProducts.findById(req.params.id)
+  let ecomproducts = await EcomProducts.findById(req.params.id);
   try {
     if (!ecomproducts) {
       res.json({
-        msg: "ecom details with this id not found"
-      })
+        msg: "ecom details with this id not found",
+      });
     } else {
-      ecomproducts= await EcomProducts.remove({ _id: req.params.id })
+      ecomproducts = await EcomProducts.remove({ _id: req.params.id });
       res.status(200).json({
         success: true,
         msg: "Delete Ecom Detail of" + req.params.id,
       });
     }
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
-  
 };
